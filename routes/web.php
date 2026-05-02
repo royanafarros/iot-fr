19189daf39003b46b1267181f095937b80c5c11f
royanafarrosm <royanafarros@gmail.com>
2026-05-02 11:41:28 +0700

    fix: update routes to use auth.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+require __DIR__.'/auth.php';
